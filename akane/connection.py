d957487341e34c4a7d6bbdb74708848a41e2d8e5
Frank Smit <frank@61924.nl>
2012-05-20 15:01:17 +0200

    First version of Python-based reply parser.

--- akane/connection.py
+++ akane/connection.py
@@ -25,6 +25,76 @@ else:
     DELIMITER = b'\r\n'
 
 
+
+class ReplyParser(object):
+    def __init__(self):
+        self._mb_count = 0
+        self._buffer = False # False means empty
+        self._read_next = False
+        self._continue_bulk = False
+
+    def feed(self, data):
+        t = data[0]
+
+        if t == '*':
+            d = int(data[1:-2])
+            if d != '-1':
+                self._read_next = True
+                self._mb_count = d
+            else:
+                self._read_next = False
+                self._buffer = None
+        elif t in '+:':
+            reply = data[1:-2]
+            if t == ':':
+                reply = int(reply)
+
+            if self._mb_count > 0:
+                self._buffer.append(reply)
+                self._mb_count -= 1
+                self._read_next = True
+            else:
+                self._buffer = reply
+                self._read_next = False
+        elif t == '$':
+            d = int(data[1:-2])
+            if d != '-1':
+                self._read_next = d + 2
+                self._continue_bulk = True
+            else:
+                reply = None
+
+                if self._mb_count > 0:
+                    self._buffer.append(reply)
+                    self._mb_count -= 1
+                    self._read_next = True
+                else:
+                    self._buffer = reply
+                    self._read_next = False
+        elif self._continue_bulk is True:
+            reply = data[:-2]
+            self._continue_bulk = False
+            if self._mb_count > 0:
+                self._buffer.append(reply)
+                self._mb_count -= 1
+                self._read_next = True
+            else:
+                self._buffer = reply
+                self._read_next = False
+
+    def read_next(self):
+        return self._read_next
+
+    def gets(self):
+        if self._buffer is False:
+            return False
+        try:
+            return self._buffer
+        finally:
+            self._buffer = False
+
+
+
 class Connection(object):
 
     _busy = False
@@ -37,7 +107,8 @@ class Connection(object):
         self.encoding = encoding
         self.encoding_errors = encoding_errors
 
-        self._reader = hiredis.Reader()
+        # self._reader = hiredis.Reader()
+        self._parser = ReplyParser()
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
@@ -70,19 +141,33 @@ class Connection(object):
         self._stream.read_until(DELIMITER, self._handle_read)
 
     def _handle_read(self, data):
-        self._reader.feed(data)
-        # print('%r' % data)
+        # self._reader.feed(data)
+        self._parser.feed(data)
 
-        response = self._reader.gets()
-        if response is not False:
+        next = self._parser.read_next()
+        if next is True:
+            self._stream.read_until(DELIMITER, self._handle_read)
+        elif next > 0:
+            self._stream.read_bytes(next, self._handle_read)
+        else: # if next is False
+            data = self._parser.gets()
             self._busy = False
             cb = self._callback
             self._callback = None
             if cb is not None:
-                cb(response)
+                cb(data)
             return
 
-        self._stream.read_until(DELIMITER, self._handle_read)
+        # response = self._reader.gets()
+        # if response is not False:
+        #     self._busy = False
+        #     cb = self._callback
+        #     self._callback = None
+        #     if cb is not None:
+        #         cb(response)
+        #     return
+
+        # self._stream.read_until(DELIMITER, self._handle_read)
 
 
 
